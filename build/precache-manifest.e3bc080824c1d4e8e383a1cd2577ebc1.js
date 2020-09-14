3e3a8723b0921028add04774e53f3cf847b911d2
Timothy Merritt <timmybytes@gmail.com>
2020-09-13 19:14:27 -0700

    Minor updates

---
+++ build/precache-manifest.e3bc080824c1d4e8e383a1cd2577ebc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d38c02d92f21d89d6f272441b4dda0",
+    "url": "/color-chooser/index.html"
+  },
+  {
+    "revision": "b01f77b9ce4ff5cbeec9",
+    "url": "/color-chooser/static/css/main.ebda9408.chunk.css"
+  },
+  {
+    "revision": "5ba1d7988c44b5db0765",
+    "url": "/color-chooser/static/js/2.22c0c28a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-chooser/static/js/2.22c0c28a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b01f77b9ce4ff5cbeec9",
+    "url": "/color-chooser/static/js/main.c0fee8d0.chunk.js"
+  },
+  {
+    "revision": "1c466d00e77dfae7ddf6",
+    "url": "/color-chooser/static/js/runtime-main.3518d3dc.js"
+  }
+]);

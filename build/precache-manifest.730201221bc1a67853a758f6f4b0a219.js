e292cd60fb47908d2ce4e142049c849bb4c4a7e7
Timothy Merritt <timmybytes@gmail.com>
2020-09-13 22:42:55 -0700

    Fix README screenshot

--- build/precache-manifest.730201221bc1a67853a758f6f4b0a219.js
+++ build/precache-manifest.730201221bc1a67853a758f6f4b0a219.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "82d38c02d92f21d89d6f272441b4dda0",
+    "revision": "a08a81377e72c09309dcfe0d43e6aabe",
     "url": "/color-chooser/index.html"
   },
   {
-    "revision": "b01f77b9ce4ff5cbeec9",
-    "url": "/color-chooser/static/css/main.ebda9408.chunk.css"
+    "revision": "dff0a827e51e5dd5a4bb",
+    "url": "/color-chooser/static/css/main.39f86355.chunk.css"
   },
   {
     "revision": "5ba1d7988c44b5db0765",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/color-chooser/static/js/2.22c0c28a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b01f77b9ce4ff5cbeec9",
-    "url": "/color-chooser/static/js/main.c0fee8d0.chunk.js"
+    "revision": "dff0a827e51e5dd5a4bb",
+    "url": "/color-chooser/static/js/main.e0ed4111.chunk.js"
   },
   {
     "revision": "1c466d00e77dfae7ddf6",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/color-chooser/precache-manifest.e3bc080824c1d4e8e383a1cd2577ebc1.js"
+  "/color-chooser/precache-manifest.730201221bc1a67853a758f6f4b0a219.js"
 );
 
 self.addEventListener('message', (event) => {

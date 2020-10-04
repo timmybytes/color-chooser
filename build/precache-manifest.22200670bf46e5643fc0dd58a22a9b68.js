0b058052106fc361cec2ed5ec2ec2b68222a0694
Timothy Merritt <timmybytes@gmail.com>
2020-10-04 13:56:49 -0700

    Add copy functionality to card component

---
+++ build/precache-manifest.22200670bf46e5643fc0dd58a22a9b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffff87d227925bb99e8539f310d70330",
+    "url": "/color-chooser/index.html"
+  },
+  {
+    "revision": "989fb2b2fefce1764ff9",
+    "url": "/color-chooser/static/css/main.01d06b7b.chunk.css"
+  },
+  {
+    "revision": "5ba1d7988c44b5db0765",
+    "url": "/color-chooser/static/js/2.22c0c28a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-chooser/static/js/2.22c0c28a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "989fb2b2fefce1764ff9",
+    "url": "/color-chooser/static/js/main.e9a0bf3d.chunk.js"
+  },
+  {
+    "revision": "1c466d00e77dfae7ddf6",
+    "url": "/color-chooser/static/js/runtime-main.3518d3dc.js"
+  }
+]);
